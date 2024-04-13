396da425e202cbb602d052ce15dec56f2f81b5e0
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-13 23:31:12 +0800

    filtering

--- UsersView.swift
+++ UsersView.swift
@@ -9,15 +9,39 @@ import SwiftData
 import SwiftUI
 
 struct UsersView: View {
-    
     @Query var users : [User]
-    
-   
+    @Environment(\.modelContext) var modelContext
     
     var body: some View {
         List(users){ user in
-            Text(user.name)
+            HStack{
+                Text(user.name)
+                
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
         }
+//        .onAppear(perform: {
+//            addSample()
+//        })
+    }
+    func addSample(){
+        let user1 = User(name: "Aoi Kano", city: "Kobe", joinDate: .now)
+        let job1 = Job(name: "Bitching", priority: 1)
+        let job2 = Job(name: "Liar", priority: 10)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
